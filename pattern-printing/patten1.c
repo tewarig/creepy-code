37d7d9150663ddc769b7e3730ee035349adef577
tewarig <gauravtewari@techie.com>
2020-02-13 22:20:39 +0530

    pattern printing problems

--- pattern-printing/patten1.c
+++ pattern-printing/patten1.c
@@ -1,25 +1,26 @@
-//in this program we will print
-//A
-//ABA
-//ABCBA
-//ABCDCBA
-//ABCDEDCBA
+/*In this program we will print
+A
+ABA
+ABCBA
+ABCDCBA
+ABCDEDCBA
+*/
 
-#include<stdio.h>
-void main()
- {
- char a= 'A';
- for(int i=0;i<5;i++)
- { 
-  for(int j=0;j<i;j++)
- {
-  printf("%c",a+j);
-  }
-  for(int k=i;k>=0;k--)
- {printf("%c",a+k);
+ #include<stdio.h>
+ void main()
+  {
+  char a= 'A';
+  for(int i=0;i<5;i++)
+  { 
+   for(int j=0;j<i;j++)
+  {
+   printf("%c",a+j);
+   }
+   for(int k=i;k>=0;k--)
+    { printf("%c",a+k);
    }
  
   
-  printf("\n");}
+   printf("\n");}
   
  }
