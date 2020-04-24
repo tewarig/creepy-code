b2380ae8c6ae5504ea01c1dde52bd930c235be0c
tewarig <gauravtewari@techie.com>
2020-04-24 22:44:55 +0530

    basic functions

---
+++ python/code_snippet/win_percentage.py
@@ -0,0 +1,10 @@
+# Write your win_percentage function here:
+def win_percentage(win,losses):
+   total = win+losses
+   num= win/total
+   return num*100
+# Uncomment these function calls to test your win_percentage function:
+print(win_percentage(5, 5))
+# should print 50
+print(win_percentage(10, 0))
+# should print 100
