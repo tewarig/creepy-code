22963ea0f786064a93d10b05bf0ae4958892185b
Gaurav Tewari <gauravtewari111@gmail.com>
2020-06-12 00:04:49 +0530

    .

---
+++ java/variables/booleanVariable.java
@@ -0,0 +1,10 @@
+public class Booleans {
+	public static void main(String[] args) {    
+    boolean intsCanHoldDecimals = false;
+    System.out.println(intsCanHoldDecimals);
+
+
+	}
+}
+
+
---
+++ java/variables/finalExample.java
@@ -0,0 +1,12 @@
+public class MyProfile {
+	public static void main(String[] args) {   
+ String name = "Gaurav";
+ int age = 19;
+ double desiredSalary = 99999999.99;
+ char gender = 'm';
+ boolean  lookingForJob = true;
+
+
+
+	}
+}
