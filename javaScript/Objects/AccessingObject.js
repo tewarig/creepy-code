41c3df4e667c3eba6c9781d1d9fad24312db31fd
Gaurav Tewari <gauravtewari111@gmail.com>
2020-06-24 00:27:10 +0530

    .

---
+++ javaScript/Objects/AccessingObject.js
@@ -0,0 +1,11 @@
+let spaceship = {
+  homePlanet: 'Earth',
+  color: 'silver',
+  'Fuel Type': 'Turbo Fuel',
+  numCrew: 5,
+  flightPath: ['Venus', 'Mars', 'Saturn']
+};
+
+// Write your code below
+var crewCount =spaceship.numCrew;
+var planetArray = spaceship.flightPath;
