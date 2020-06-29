c9d5736ad8df3846d2499c57c7da9e84bdc6df19
Gaurav Tewari <gauravtewari111@gmail.com>
2020-06-29 23:23:05 +0530

    boilerPlate Code added

---
+++ DsAndAlgo/O_N/O_1.js
@@ -0,0 +1,11 @@
+const array = ['cool','element','something'];
+function firstTwoBoxes(boxes)
+{
+  console.log(boxes[0]); //O(n)
+  console.log(boxes[1]); //o(n)
+
+}
+
+firstTwoBoxes(array);
+//flat graph
+// o(1)
