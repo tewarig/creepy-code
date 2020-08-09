6a4311f5ebff810cee012de785beca95c4e4a806
Gaurav Tewari <54894783+tewarig@users.noreply.github.com>
2020-08-09 12:43:33 +0530

    answer 3 updated

--- c/assignment2/answer3.cpp
+++ c/assignment2/answer3.cpp
@@ -1,82 +1,56 @@
 #include<iostream>
-#include<stdio.h>
 using namespace std;
-
-class account {
-private:
-	int accountBalance;
+class Date
+{
+    int day;
+    int month;
+    int year;
 public:
-	account(int balance) {
-		if (balance > 0)
-		{
-			accountBalance = balance;
-		} else {
-			cout << "Balace can't be negative.. setting it to zero \n";
-			balance = 0;
-			accountBalance = balance;
-
-		}
-	}
-	void Credit(int amount)
-	{
-		accountBalance += amount;
-
-	}
-	void Debit (int amount)
-	{
-		int temp = accountBalance - amount;
-		if (temp > 0)
-		{
-			accountBalance -= amount;
-
-		} else {
-			cout << "Debit amount exceeded account balance. \n";
-		}
-	}
-	int getBalance ()
-	{
-		return accountBalance;
-	}
-
+    Date()
+    {   int a,b,c;
+        cout<<"Enter the date,month and year";
+        cin>>a>>b>>c;
+        day=a;
+        month=b;
+        year=c;
+        if(month<1||month>12)
+        month=1;
+    }
+   int setday()
+   {
+       int d;
+       cout<<"Enter the date";
+      cin>>d;
+       day=d;
+   return 0;
+   }
+   int getmonth()
+   {   int e;
+       cout<<"Enter the month";
+       cin>>e;
+       month=e;
+       return 0;
+   }
+   int getyear()
+   {   int f;
+       cout<<"Enter the year";
+       cin>>f;
+       year=f;
+       return 0;
+   }
+    int displayDate()
+    {
+       cout<<"the date is:";
+        cout<<month<<"/"<<day<<"/"<<year;
+        return 0;
+    }
 };
 int main()
 {
-/*
-#ifndef ONLINE_JUDGE
-	freopen("input.txt" , "r" , stdin);
-	freopen("output.txt", "w" , stdout);
-#endif
-*/
-	int openBalance;
-	cout << "enter the amount you want to start binod account with? \n";
-	cin >> openBalance;
-	account binod(openBalance);
-	int paisa;
-	cout << "enter the amount you want to add in binod's account? \n";
-	cin >> paisa;
-	binod.Credit(paisa);
-	cout << binod.getBalance() << "\n";
-	double paisa1 ;
-	cout << "enter the amount you want to removein binod's account? \n";
-	cin >> paisa1;
-	binod.Debit(paisa1);
-	cout << binod.getBalance() << "\n";
-	cout << "enter the amount you want to start Gaurav account with? \n";
-	cin >> openBalance;
-	account gaurav(openBalance);
-	cout << "enter the amount you want to add in Gaurav's account? \n";
-	cin >> paisa;
-	gaurav.Debit(paisa);
-	cout << gaurav.getBalance() << "\n";
-	cout << "enter the amount you want to removein Gaurav's account? \n";
-	cin >> paisa1;
-	gaurav.Debit(paisa1);
-	cout << gaurav.getBalance() << "\n";
-
-
-
-
-
-
-	return 0;
+    Date ob;
+    ob.setday();
+    ob.getmonth();
+    ob.getyear();
+    ob.displayDate();
+    return 0;
 }
